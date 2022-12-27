476e288d9b93772e85bd14470bef5f5e35baa620
master <master.code.develop@gmail.com>
2022-12-27 19:28:43 +0100

    DAJR-54 reset service api

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from 'react';
 
 export default function Router() {
-  return (
-    <div>Router</div>
-  )
+  return <div>Router</div>;
 }
---
+++ frontend/src/services/api.js
@@ -0,0 +1,1 @@
+const URL_API = 'http://localhost:3001/api/v1/';
