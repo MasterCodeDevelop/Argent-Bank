b9cac5fc1e3fd1b0c28ed6a0bc14e92d77bed4f6
master <master.code.develop@gmail.com>
2022-12-27 23:05:39 +0100

    DAJR-57 add error page 404

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import iconInfinity from '../assets/svg/icon-infinity.svg';
 export default function Error() {
-  return <div>Error</div>;
+  return (
+    <main className="error-page">
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n&apos;existe pas.</p>
+      <img src={iconInfinity} alt="Infinity animation" />
+      <Link to="/">Retourner sur la page d&apos;accueil</Link>
+    </main>
+  );
 }
