792a415633431ce56d393304a59fb358066788ff
master <master.code.develop@gmail.com>
2022-12-27 21:30:50 +0100

    initialize header (SCSS/JSX)

--- frontend/src/layouts/Header.jsx
+++ frontend/src/layouts/Header.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../assets/img/argentBankLogo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle, faSignIn } from '@fortawesome/free-solid-svg-icons';
 export default function Header() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/sign-up">SignUp</Link>
-          </li>
-          <li>
-            <Link to="/sign-in">SignIn</Link>
-          </li>
-          <li>
-            <Link to="/user">User</Link>
-          </li>
-        </ul>
+    <header className="header">
+      <Link className="header-logo" to="/">
+        <img src={logo} alt="Argent Bank Logo" />
+        <h1 className="sr-only">Argent Bank</h1>
+      </Link>
+      <nav className="header-nav">
+        <NavLink to="./sign-up">
+          <FontAwesomeIcon icon={faSignIn} /> Sign Up
+        </NavLink>
+        <NavLink to="./sign-in">
+          <FontAwesomeIcon icon={faUserCircle} /> Sign In
+        </NavLink>
       </nav>
-    </div>
+    </header>
   );
 }
