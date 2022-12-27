f37e0aff6123e5e6380f93909f82c9df601bf34b
master <master.code.develop@gmail.com>
2022-12-27 22:40:44 +0100

    DAJR-56 add footer

--- frontend/src/layouts/Footer.jsx
+++ frontend/src/layouts/Footer.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <p className="footer-text">Copyright 2020 Argent Bank</p>
+    </footer>
+  );
 }
