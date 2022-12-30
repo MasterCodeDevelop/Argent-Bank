9ede228a22e07d6b8e196c6e3a423a46644fa1b9
master <master.code.develop@gmail.com>
2022-12-30 22:40:44 +0100

    add Welcome component on Profile page

---
+++ frontend/src/components/Welcome.jsx
@@ -0,0 +1,126 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { clearMessage } from '../slices/message';
+import { useSelector, useDispatch } from 'react-redux';
+
+function Welcome({ firstName, lastName }) {
+  const [edit, setEdit] = useState(false);
+  const initialValues = {
+    firstName: '',
+    lastName: '',
+  };
+  const [successful, setSuccessful] = useState(false);
+  const { message } = useSelector((state) => state.message);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(clearMessage());
+  }, [dispatch]);
+  const validationSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .test(
+        'len',
+        'The first name must be between 3 and 20 characters.',
+        (val) =>
+          val && val.toString().length >= 3 && val.toString().length <= 20
+      )
+      .required('This field is required!'),
+    lastName: Yup.string()
+      .test(
+        'len',
+        'The last name must be between 3 and 20 characters.',
+        (val) =>
+          val && val.toString().length >= 3 && val.toString().length <= 20
+      )
+      .required('This field is required!'),
+  });
+  const handleEdit = (formValue) => {
+    const { firstName, lastName } = formValue;
+    console.log({ firstName, lastName });
+    setSuccessful(false);
+  };
+  return (
+    <section className="welcome">
+      <h1>
+        Welcome back
+        <br />
+        {edit ? <></> : <p>{firstName + '  ' + lastName + ' !'}</p>}
+      </h1>
+
+      {!edit ? (
+        <button className="btn btn-secondary" onClick={() => setEdit(true)}>
+          Edit
+        </button>
+      ) : (
+        <>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleEdit}
+          >
+            <Form>
+              {!successful && (
+                <>
+                  <div className="form-group">
+                    <Field
+                      name="firstName"
+                      type="text"
+                      placeholder={firstName}
+                      className="form-control"
+                    />
+                    <ErrorMessage
+                      name="firstName"
+                      component="div"
+                      className="alert alert-danger"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <Field
+                      name="lastName"
+                      type="text"
+                      placeholder={lastName}
+                      className="form-control"
+                    />
+                    <ErrorMessage
+                      name="lastName"
+                      component="div"
+                      className="alert alert-danger"
+                    />
+                  </div>
+                  <div className="form-group">
+                    {message && (
+                      <div className="alert alert-danger" role="alert">
+                        {message}
+                      </div>
+                    )}
+                    <div className="row">
+                      <button
+                        type="submit"
+                        className="btn btn-outline-secondary"
+                      >
+                        Save
+                      </button>
+                      <button
+                        type="reset"
+                        onClick={() => setEdit(false)}
+                        className="btn btn-outline-secondary"
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </div>
+                </>
+              )}
+            </Form>
+          </Formik>
+        </>
+      )}
+    </section>
+  );
+}
+Welcome.propTypes = {
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+};
+export default Welcome;
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Loading from '../components/Loading';
+import Welcome from '../components/Welcome';
 
 export default function Profile() {
   const { user } = useSelector((state) => state.user);
 
   return (
     <main id="profile" className="profile">
-      {!user ? <Loading /> : <>USER:{user.firstName}</>}
+      {!user ? (
+        <Loading />
+      ) : (
+        <>
+          <Welcome firstName={user.firstName} lastName={user.lastName} />
+        </>
+      )}
     </main>
   );
 }
